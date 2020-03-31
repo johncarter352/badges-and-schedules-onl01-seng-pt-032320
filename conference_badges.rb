daf79a8474985a8fc325e389a94481effe3d7531
John Carter <johncarter352@gmail.com>
2020-03-31 04:13:57 +0000

    Automatically backed up by Learn

--- conference_badges.rb
+++ conference_badges.rb
@@ -12,7 +12,8 @@ end
 def assign_rooms(array)
   n_array = []
   room = 1 
-  array.each_with_index{|name, room| n_array << "Hello, #{name}! You'll be assigned to room #{room}!"} room +=1
+  array.each_with_index{|name, room| n_array << "Hello, #{name}! You'll be assigned to room #{room}!"} 
+  room +=1
   n_array
 end
 
