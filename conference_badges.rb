93b5152512bcb1a78837ee3069973bdd496a6fa0
John Carter <johncarter352@gmail.com>
2020-03-31 03:45:56 +0000

    Automatically backed up by Learn

--- conference_badges.rb
+++ conference_badges.rb
@@ -1,1 +1,9 @@
-# Write your code here.
+def badge_maker(array)
+    return "Hello, my name is #{array}."
+end
+
+
+def batch_badge_creator(array)
+  names = ["Edsger", "Ada", "Charles", "Alan", "Grace", "Linus", "Matz"]
+  names.each{|name| names << "Hello, my name is #{name}."}
+end
